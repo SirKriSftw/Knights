4e3f540f0604dee37ee63b26410e5547992bd7eb
Kristian Perez <kpere192@fiu.edu>
2024-04-22 18:11:02 -0400

    Made a simple function for knight class that finds all possible squares it can go to given a position and board size

--- lib/Knight.rb
+++ lib/Knight.rb
@@ -1,7 +1,29 @@
 class Knight
-  attr_accessor: :pos
-  
+  attr_accessor :pos
+  BOARD_DIMENSION = 8
+
   def initialize(pos)
     @pos = pos
+  end
+
+  def possible_moves(pos = @pos)
+    x = pos[0]
+    y = pos[1]
+    positions = [
+      [x + 2, y + 1],
+      [x + 2, y - 1],
+      [x - 2, y + 1],
+      [x - 2, y - 1],
+      [x + 1, y + 2],
+      [x - 1, y + 2],
+      [x + 1, y - 2],
+      [x - 1, y - 2],
+    ]
+
+    possible_pos = positions.filter do |value|
+      value[0] >= 0 && value[0] <= BOARD_DIMENSION && value[1] >= 0 && value[1] <= BOARD_DIMENSION
+    end
+
+    possible_pos
   end
 end
---
+++ lib/main.rb
@@ -0,0 +1,5 @@
+require_relative "Knight"
+
+knight = Knight.new([1,2])
+
+p knight.possible_moves
